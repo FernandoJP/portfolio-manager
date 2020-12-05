0eea3f53418a5e01b405f3639d257e598f5da284
Fernando de Paula <fernando.opus@gmail.com>
2020-12-05 11:39:47 -0300

    Returns reducer and action

---
+++ src/store/reducers/returnChart.js
@@ -0,0 +1,17 @@
+import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
+import returnsMock from '../../assets/mock/returns.json';
+
+const initialState = returnsMock;
+
+const setReturns( state, action ) {
+    return state;
+}
+
+const reducer = (state = initialState, action ) => {
+    switch( action.type ) {
+        case actionTypes.SET_RETURNS: return setReturns( state, action );
+    }
+}
+
+export default reducer;
