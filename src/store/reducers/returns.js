a482ae737eb02fc9d5aa350fa9056b97e0080912
Fernando de Paula <fernando.opus@gmail.com>
2020-12-08 13:18:25 -0300

    Dynamic data on the chart

--- src/store/reducers/returns.js
+++ src/store/reducers/returns.js
@@ -2,15 +2,21 @@ import * as actionTypes from '../actions/actionTypes';
 import { updateObject } from '../utility';
 import returnsMock from '../../assets/mock/returns.json';
 
-const initialState = returnsMock;
+const initialState = {
+    returnsData: Object.values(returnsMock.data)[0]
+}
 
 const setReturns = ( state, action ) => {
-    return state;
+    const updatedState = {
+        returnsData: action.returnsData
+    }
+    return updateObject( state, updatedState );
 }
 
 const reducer = (state = initialState, action ) => {
     switch( action.type ) {
         case actionTypes.SET_RETURNS: return setReturns( state, action );
+        default: return state;
     }
 }
 
